950f53928007fc66cbdec7c8f1d7bdd3d65d963b
unknown <hilow0090@gmail.com>
2023-11-15 16:19:27 -0800

    finished home page and form templates

--- app.py
+++ app.py
@@ -5,5 +5,16 @@ from stories import *
 
 app = Flask(__name__)
 
+storyDict = {'story1': story1, "story2":story2, 'story3':story3}
+
 @app.route('/')
 def show_home():
+  return render_template('home.html')
+
+@app.route('/form/<story_id>')
+def show_form(story_id):
+  return render_template('form.html', story= storyDict.get(story_id))
+
+@app.route('/form/<story_id>/results')
+def show_results(story_id):
+  return render_template('form.html', story= storyDict.get(story_id))
--- stories.py
+++ stories.py
@@ -38,8 +38,24 @@ class Story:
 # Here's a story to get you started
 
 
-story = Story(
+story1 = Story(
     ["place", "noun", "verb", "adjective", "plural_noun"],
     """Once upon a time in a long-ago {place}, there lived a
        large {adjective} {noun}. It loved to {verb} {plural_noun}."""
+)
+
+story2 = Story(
+    ["adjective", "plural_food_1", 'verb', 'noun', 'plural_food_2', 'color', 'animal', 'something_you_can_ride_in'],
+    """Today I went to my favorite Taco Stand called the {adjective} {animal}.
+    Unlike most food stands, they cook and prepare the food in a {something_you_can_ride_in}
+     while you {verb}. The best thing on the menu is the {color} {noun}. Instead of ground beef
+     they fill the taco with {plural_food_2}, cheese, and top it off with a salsa made from {plural_food_1}."""
+)
+
+story3 = Story(
+    ['verb', 'adjective_1', 'adjective_2', 'place', 'color', 'time_of_day', 'food_1', 'food_2', 'plural_noun', 'number'],
+    """Bats are so cool! They are {color}, {adjective_2} animals which have wings. They like to fly around at
+     {time_of_day} which makes some people scared of them. But bats are {adjective_1}, and they don't want to
+     hurt people. I have a pet bat that lives in {place}. I like to feed him {food_1} and {food_2}. He likes
+     to {verb}. I am his favorite person, but he also likes {plural_noun}. I want to convince my parents to get me {number} more bats."""
 )
